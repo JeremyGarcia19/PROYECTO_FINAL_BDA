b6c1b124194d9e0612d2d3a84fd00ab06947a442
Jeremy García <jeremy.garcia.meneses19@gmail.com>
2023-01-21 13:39:16 -0600

    Actualización Carga de datos

---
+++ s-38-carga-status-dispositivo.sql
@@ -0,0 +1,5 @@
+insert into STATUS_DISPOSITIVO (STATUS_DISPOSITIVO_ID, CLAVE, DESCRICION) values (1, 'VIGENTE', 'mi nulla ac enim in tempor turpis nec euismod scelerisque quam turpis adipiscing lorem vitae');
+insert into STATUS_DISPOSITIVO (STATUS_DISPOSITIVO_ID, CLAVE, DESCRICION) values (2, 'OBSOLETO', 'eget semper rutrum nulla nunc purus phasellus in felis donec semper sapien a libero nam dui');
+insert into STATUS_DISPOSITIVO (STATUS_DISPOSITIVO_ID, CLAVE, DESCRICION) values (3, 'AVERIADO', 'ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus metus arcu adipiscing molestie');
+insert into STATUS_DISPOSITIVO (STATUS_DISPOSITIVO_ID, CLAVE, DESCRICION) values (4, 'EN_REPARACION', 'eu felis fusce posuere felis sed lacus morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed');
+insert into STATUS_DISPOSITIVO (STATUS_DISPOSITIVO_ID, CLAVE, DESCRICION) values (5, 'FUERA_DE_USO', 'non ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus metus arcu adipiscing molestie hendrerit');
